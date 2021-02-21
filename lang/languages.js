7cc87d5e3e9a5268f6474bd9344d2b1e35bfa6b5
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2021-02-21 21:46:30 +0000

    deploy: B3T4C0R3/blaes@94129e3aa7087bdd1787403615509976563fe5ac

--- lang/languages.js
+++ lang/languages.js
@@ -1,9 +1,18 @@
-export default [
-  {
-    code: 'nl',
-    iso: 'nl-NL',
-    name: 'Dutch',
-    file: 'nl-NL.js',
-    dir: 'ltr'
-  }
-]
+import axios from 'axios'
+export default async function() {
+  return await axios
+    .get(`https://api.betacore.nl/language/5fcbaef0da2e2ee081cb7425`)
+    .then(res => {
+      var languages = []
+      res.data.forEach(language => {
+        languages.push({
+          code: language.code,
+          iso: language.iso,
+          name: language.name,
+          dir: language.dir,
+          file: 'translations.js'
+        })
+      })
+      return languages
+    })
+}
